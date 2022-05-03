ece9c44a2f6b956d32f8d041d3ecb3a471945e32
PatoGM <patriciogm@live.com>
2022-05-03 09:28:15 -0500

    added replies to bot commands

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // Require the necessary discord.js classes
 const discord_js_1 = require("discord.js");
@@ -9,5 +18,19 @@ const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GU
 client.once('ready', () => {
     console.log('Ready!');
 });
+client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!interaction.isCommand())
+        return;
+    const { commandName } = interaction;
+    if (commandName === 'ping') {
+        yield interaction.reply('Pong!');
+    }
+    else if (commandName === 'server') {
+        yield interaction.reply('Server info.');
+    }
+    else if (commandName === 'user') {
+        yield interaction.reply('User info.');
+    }
+}));
 // Login to Discord with your client's token
 client.login(config_json_1.token);
