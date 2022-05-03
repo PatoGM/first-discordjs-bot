35c0a7497868ee507f6670fffb47e8e94f2d0511
PatoGM <patriciogm@live.com>
2022-05-03 08:52:44 -0500

    Fixed tsc setup, added tsconfig, and added more to readme

--- README.md
+++ README.md
@@ -10,6 +10,17 @@
 	"token": "token goes here"
 }
 
-4. Open one terminal inside the folder you just cloned and run "tsc --watch index.ts"
+4. Open one terminal inside the folder you just cloned and run "tsc -w"
 5. Open another terminal and run "node index.js" whenever you want to run the bot.
 6. Code away!
+
+## Commands used to setup this repo
+
+1. npm init
+2. npm install discord.js
+- This is the bare minimum to have a "bot", although it won't do anything
+3. npm install @discordjs/builders @discordjs/rest discord-api-types
+- Allows for bare minimum bot "slash commands"
+4. tsc --init
+5. Uncomment tsconfig line 36 ("resolveJsonModule": true,) to remove tsc errors
+- Can probably do this earlier!
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 // Require the necessary discord.js classes
-var discord_js_1 = require("discord.js");
-var config_json_1 = require("./config.json");
+const discord_js_1 = require("discord.js");
+const config_json_1 = require("./config.json");
 // Create a new client instance
-var client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
+const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
-client.once('ready', function () {
+client.once('ready', () => {
     console.log('Ready!');
 });
 // Login to Discord with your client's token
