fe17770be599b2a84645f601b73f747fada59554
PatoGM <patriciogm@live.com>
2022-05-03 09:47:06 -0500

    reordered stuff trying to debug why not all commands show

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,12 +5,12 @@ const rest_1 = require("@discordjs/rest");
 const v9_1 = require("discord-api-types/v9");
 const config_json_1 = require("./config.json");
 const commands = [
-    new builders_1.SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new builders_1.SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
     new builders_1.SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new builders_1.SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+    new builders_1.SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 ]
     .map(command => command.toJSON());
 const rest = new rest_1.REST({ version: '9' }).setToken(config_json_1.token);
 rest.put(v9_1.Routes.applicationGuildCommands(config_json_1.clientId, config_json_1.guildId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
+    .then((resp) => console.log('Successfully registered application commands.', resp))
     .catch(console.error);
--- deploy-commands.ts
+++ deploy-commands.ts
@@ -4,14 +4,14 @@ import { Routes } from 'discord-api-types/v9';
 import { clientId, guildId, token } from './config.json';
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then((resp) => console.log('Successfully registered application commands.', resp))
 	.catch(console.error);
